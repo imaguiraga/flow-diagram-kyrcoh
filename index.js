b5f06111a32bc14207e31a347f12785982f395f0
imaguiraga <imaguiraga@gmail.com>
2020-02-22 10:47:26 -0500

    1 file modified, 2 added, 3 deleted

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {
   Terminal,
   flowgraph,
   G6Visitor
-} from "./flow.js";
+} from "./src/flow.js";
 
 let selectClause = () => sequence(a, b, repeat(optional("c")), zeroOrMore("d"));
 let fromClause = () => choice("1", "2", selectClause, "4");
