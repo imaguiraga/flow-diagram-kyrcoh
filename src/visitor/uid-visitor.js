19a40bff705c5c654605f31557608b81502e93b4
imaguiraga <imaguiraga@gmail.com>
2020-02-23 12:57:05 -0500

    1 file modified

--- src/visitor/uid-visitor.js
+++ src/visitor/uid-visitor.js
@@ -11,7 +11,8 @@ export class UIDVisitor {
       tree.start.id = this._prefix + ":" + tree.kind + ".start";
       tree.finish.id = this._prefix + ":" + tree.kind + ".finish";
     }
-    tree._nodes.filter(n => n instanceof Object).forEach((node,index) => {
+    tree._nodes.filter(n => n instanceof Object).forEach(
+      (node,index) =>  {
         // keep only terminal nodes
         let p = this._prefix.concat("."+index);
         node.id = p + ":" + node.kind;
